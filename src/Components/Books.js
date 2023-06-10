2f58e948bed57cbfbfefb0168240c09348ed7407
Shivaji Jadhav <sj461898@gmail.com>
2023-06-10 13:28:15 +0530

    Edit_functionality added

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,47 +1,47 @@
-import React, { useEffect } from 'react'
-import { useSelector , useDispatch} from 'react-redux'
-import { getbookData } from './Redux/action'
-import Filter from './Filter'
-import { useLocation, useSearchParams } from 'react-router-dom'
-
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getbookData } from './Redux/action';
+import Filter from './Filter';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 function Books() {
-  const dispatch=useDispatch()
-  const location = useLocation()
-  const [searchParams,setSearchParams] = useSearchParams()
-  const BookData=useSelector((store)=>{
-    
-    return {...store}
-  })
-  console.log(BookData,"check bookdata")
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const BookData = useSelector((store) => ({ ...store }));
+  const [ascendingOrder, setAscendingOrder] = useState('');
+  
 
-  useEffect(()=>{
-    if(location){
-      let queryParams ={
-        params:{
-          category : searchParams.getAll("category")
-        }
-      }
-    dispatch(getbookData(queryParams))
+  useEffect(() => {
+    if (location) {
+      let queryParams = {
+        params: {
+          category: searchParams.getAll('category'),
+        },
+      };
+      dispatch(getbookData(queryParams,ascendingOrder));
     }
-  },[location])
-  // console.log(BooksData,"checkBooksData")
+  }, [location,ascendingOrder]);
+
+ 
   return (
     <div>
-      <Filter/>
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
-    {BookData.Book?.map((e, index) => (
-      <div key={e.id}>
-        <img src={e.cover_photo} alt={e.book_name} height={400} />
-        <h5>Name: {e.book_name}</h5>
-        <h5>Category: {e.category}</h5>
+      <Filter />
+      <button onClick={()=>setAscendingOrder("asc")} >Sort by release_year</button>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
+        {BookData.Book?.map((e, index) => (
+          <div key={e.id}>
+            <Link to={`/books/${e.id}`}>
+              <img src={e.cover_photo} alt="Book_Name" height={400} />
+              <h5>Name: {e.book_name}</h5>
+              <h5>Category: {e.category}</h5>
+              <h5>Category: {e.release_year}</h5>
+            </Link>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-  </div>
-  
-  
-  )
+    </div>
+  );
 }
 
-export default Books
+export default Books;
--- src/Components/Redux/action.js
+++ src/Components/Redux/action.js
@@ -1,13 +1,45 @@
-import axios from "axios"
-import { GET_BOOK_FAILEIR, GET_BOOK_REQUEST, GET_BOOK_SUCCESS } from "./actionType"
+import axios from "axios";
+import {
+  EDIT_NAME_AND_YEART_FAIL,
+  EDIT_NAME_AND_YEART_REQUEST,
+  EDIT_NAME_AND_YEART_SUCCESS,
+  GET_BOOK_FAILEIR,
+  GET_BOOK_REQUEST,
+  GET_BOOK_SUCCESS,
+} from "./actionType";
 
-export const getbookData=(queryParams)=>(dispatch)=>{
-dispatch({type:GET_BOOK_REQUEST})
-   return axios.get(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/books`,queryParams).then((res)=>{
-    dispatch({type:GET_BOOK_SUCCESS,payload:res.data})
-    console.log(res.data)
-   }).catch((err)=>{
-    dispatch({type:GET_BOOK_FAILEIR})
-    console.log(err)
-   })
+export const getbookData = (queryParams, ascendingOrder) => (dispatch) => {
+  dispatch({ type: GET_BOOK_REQUEST });
+  return axios
+    .get(
+      `http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/books`,
+      queryParams
+    )
+    .then((res) => {
+      let sortData = res.data;
+      if (ascendingOrder === "asc") {
+        sortData = res.data.sort((a, b) => a.release_year - b.release_year);
+      }
+      dispatch({ type: GET_BOOK_SUCCESS, payload: sortData });
+      // console.log(res.data);
+    })
+    .catch((err) => {
+      dispatch({ type: GET_BOOK_FAILEIR,payload:err.message });
+      console.log(err);
+    });
 }
+
+export const updateData = (singleBookData, updatedBook) => (dispatch) => {
+  dispatch({type:EDIT_NAME_AND_YEART_REQUEST})
+  axios.patch(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/books/${singleBookData.id}`, updatedBook)
+    .then(res => {
+      const updatedData = { ...singleBookData, ...res.data };
+      dispatch({type:EDIT_NAME_AND_YEART_SUCCESS,payload:updateData})
+
+    })
+ .catch(error => {
+      
+      dispatch({type:EDIT_NAME_AND_YEART_FAIL,payload:error.messege})
+    });
+};
+  
